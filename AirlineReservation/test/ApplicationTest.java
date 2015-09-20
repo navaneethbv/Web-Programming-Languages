3362bc28fd01e61eaacaa9189ad17f58d4267206
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-09-20 15:12:22 -0500

    Update ApplicationTest.java

--- AirlineReservation/test/ApplicationTest.java
+++ AirlineReservation/test/ApplicationTest.java
@@ -2,7 +2,7 @@ import org.junit.*;
 
 import static org.fest.assertions.Assertions.*;
 
-/**
+/** 
  *
  * Simple (JUnit) tests that can call all parts of a play app. If you are
  * interested in mocking a whole application, see the wiki for more details.
